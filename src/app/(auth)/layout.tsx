353fb1a9cfb975d6f064e4785c10de161c813c65
amirfaisalz <amirsakti27@gmail.com>
2024-05-09 22:21:08 +0700

    check user session, if loggedin when user go to auth page, redirect to dashboard

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,7 +1,17 @@
-export default function AuthLayout({
+import { redirect } from "next/navigation";
+
+import { validateRequest } from "@/lib/lucia";
+
+export default async function AuthLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const { user } = await validateRequest();
+
+  if (user) {
+    return redirect("/dashboard");
+  }
+
   return <div className="h-screen grid place-items-center">{children}</div>;
 }
