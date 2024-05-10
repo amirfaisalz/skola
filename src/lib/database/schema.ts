0bafad1bce394dcb8733c6cf475e0672dadef6d8
amirfaisalz <amirsakti27@gmail.com>
2024-05-11 02:43:01 +0700

    add role and permissions schema

--- src/lib/database/schema.ts
+++ src/lib/database/schema.ts
@@ -1,4 +1,5 @@
 import { pgTable, text, timestamp } from "drizzle-orm/pg-core";
+import { type InferSelectModel } from "drizzle-orm";
 
 export const userTable = pgTable("user", {
   id: text("id").primaryKey(),
@@ -6,6 +7,9 @@ export const userTable = pgTable("user", {
   lastName: text("last_name").notNull(),
   email: text("email").notNull().unique(),
   hashedPassword: text("hashed_password"),
+  roleId: text("role_id")
+    .notNull()
+    .references(() => roleTable.id),
 });
 
 export const sessionTable = pgTable("session", {
@@ -17,4 +21,19 @@ export const sessionTable = pgTable("session", {
     withTimezone: true,
     mode: "date",
   }).notNull(),
+});
+
+export const roleTable = pgTable("role", {
+  id: text("id").primaryKey(),
+  name: text("name").notNull().unique(),
+});
+
+export type TRole = InferSelectModel<typeof roleTable>;
+
+export const permissionTable = pgTable("permission", {
+  id: text("id").primaryKey(),
+  roleId: text("role_id")
+    .notNull()
+    .references(() => roleTable.id), // Reference to the roles table
+  name: text("name").notNull(),
 });
