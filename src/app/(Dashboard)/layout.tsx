ecda633751594739d7411102b5e8e105a5681c9b
amirfaisalz <amirsakti27@gmail.com>
2024-06-02 03:36:21 +0700

    change layout dashboard

--- src/app/(Dashboard)/layout.tsx
+++ src/app/(Dashboard)/layout.tsx
@@ -32,95 +32,92 @@ export default async function DashboardLayout({
   }
 
   return (
-    <div className="h-screen grid place-items-center">
-      <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
-        <div className="hidden border-r bg-muted/40 md:block">
-          <div className="flex h-full max-h-screen flex-col gap-2">
-            <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
-              <Link
-                href="/dashboard"
-                className="flex items-center gap-2 font-semibold"
-              >
-                <Package2 className="h-6 w-6" />
-                <span className="">Skola</span>
-              </Link>
-              <Button variant="outline" size="icon" className="ml-auto h-8 w-8">
-                <Bell className="h-4 w-4" />
-                <span className="sr-only">Toggle notifications</span>
-              </Button>
-            </div>
-            <div className="flex-1">
-              {/* nav desktop */}
-              <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
-                <NavGroup navItems={navItems} />
-              </nav>
-            </div>
+    <div className="h-screen flex">
+      {/* <!-- Sidebar --> */}
+      <aside className="hidden md:block w-64 bg-muted/40 border-r flex-none h-screen sticky top-0">
+        <div className="flex h-full max-h-screen flex-col gap-2">
+          <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
+            <Link
+              href="/dashboard"
+              className="flex items-center gap-2 font-semibold"
+            >
+              <Package2 className="h-6 w-6" />
+              <span className="">Skola</span>
+            </Link>
+            <Button variant="outline" size="icon" className="ml-auto h-8 w-8">
+              <Bell className="h-4 w-4" />
+              <span className="sr-only">Toggle notifications</span>
+            </Button>
+          </div>
+          <div className="flex-1">
+            {/* nav desktop */}
+            <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
+              <NavGroup navItems={navItems} />
+            </nav>
           </div>
         </div>
-        <div className="flex flex-col">
-          <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
-            <div className="flex-1">
-              <Sheet>
-                <SheetTrigger asChild>
-                  <Button
-                    variant="outline"
-                    size="icon"
-                    className="shrink-0 md:hidden"
-                  >
-                    <Menu className="h-5 w-5" />
-                    <span className="sr-only">Toggle navigation menu</span>
-                  </Button>
-                </SheetTrigger>
-                <SheetContent side="left" className="flex flex-col">
-                  {/* nav mobile */}
-                  <nav className="grid gap-2 text-lg font-medium">
-                    <Link
-                      href="/dashboard"
-                      className="flex items-center gap-2 text-lg font-semibold mb-4"
-                    >
-                      <Package2 className="h-6 w-6" />
-                      <span>Skola</span>
-                    </Link>
-                    <NavGroup navItems={navItems} mobile />
-                  </nav>
-                </SheetContent>
-              </Sheet>
-            </div>
+      </aside>
 
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
+      {/* <!-- Main Content --> */}
+      <div className="flex-1 flex flex-col overflow-hidden">
+        {/* <!-- Header --> */}
+        <header className="flex bg-muted-40 h-14 border-b lg:px-6 lg:h-[66px] px-4 sticky top-0 z-10 items-center gap-4">
+          <div className="flex-1">
+            <Sheet>
+              <SheetTrigger asChild>
                 <Button
-                  variant="secondary"
+                  variant="outline"
                   size="icon"
-                  className="rounded-full"
+                  className="shrink-0 md:hidden"
                 >
-                  <CircleUser className="h-5 w-5" />
-                  <span className="sr-only">Toggle user menu</span>
+                  <Menu className="h-5 w-5" />
+                  <span className="sr-only">Toggle navigation menu</span>
                 </Button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent align="end">
-                <DropdownMenuLabel>My Account</DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem>Settings</DropdownMenuItem>
-                <DropdownMenuItem>Support</DropdownMenuItem>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem>
-                  <form action={signOut}>
-                    <Button type="submit" className="w-full">
-                      Sign out
-                    </Button>
-                  </form>
-                </DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
+              </SheetTrigger>
+              <SheetContent side="left" className="flex flex-col">
+                {/* nav mobile */}
+                <nav className="grid gap-2 text-lg font-medium">
+                  <Link
+                    href="/dashboard"
+                    className="flex items-center gap-2 text-lg font-semibold mb-4"
+                  >
+                    <Package2 className="h-6 w-6" />
+                    <span>Skola</span>
+                  </Link>
+                  <NavGroup navItems={navItems} mobile />
+                </nav>
+              </SheetContent>
+            </Sheet>
+          </div>
 
-            <ToggleTheme />
-          </header>
-          <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
-            {/* children */}
-            {children}
-          </main>
-        </div>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="secondary" size="icon" className="rounded-full">
+                <CircleUser className="h-5 w-5" />
+                <span className="sr-only">Toggle user menu</span>
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Settings</DropdownMenuItem>
+              <DropdownMenuItem>Support</DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>
+                <form action={signOut}>
+                  <Button type="submit" className="w-full">
+                    Sign out
+                  </Button>
+                </form>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+
+          <ToggleTheme />
+        </header>
+
+        {/* <!-- Content --> */}
+        <main className="overflow-y-auto p-4 lg:p-6 h-full">{children}</main>
       </div>
     </div>
   );
